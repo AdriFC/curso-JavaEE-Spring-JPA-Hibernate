cd9d417b84b166fea4868623f27669ba31ab34dc
Adri <adri86vigo@gmail.com>
2024-09-18 18:26:28 +0200

    SPRING - Ejercicio vid 55 (Buscardor de cursos I)

---
+++ 20-buscador_cursos_spring/src/main/java/com/formacion/controller/CursosController.java
@@ -0,0 +1,44 @@
+package com.formacion.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.formacion.model.Curso;
+
+@Controller
+public class CursosController {
+	
+	List<Curso> cursos;
+	
+	//Constructor
+	public CursosController() {
+		cursos = new ArrayList<>();
+		cursos.add(new Curso("Java", "Programación", 50));
+		cursos.add(new Curso("Angular", "Programación", 30));
+		cursos.add(new Curso("Linux", "Sistemas", 40));
+		cursos.add(new Curso("Big Data", "Datos", 30));
+		cursos.add(new Curso("SQL", "Datos", 20));
+	}
+	
+	@PostMapping(value="buscar")
+	public String buscador(@RequestParam("tema") String tema, HttpServletRequest request) {
+		
+		//Búsqueda
+		List<Curso> resultado = cursos.stream()
+				.filter(c->c.getTema().equals(tema))
+				.collect(Collectors.toList());
+		//Guardado de la búsqueda en atributo de petición
+		request.setAttribute("cursos", resultado);
+		
+		//Rediccionamiento a la vista
+		return "cursos";
+	}
+	
+}

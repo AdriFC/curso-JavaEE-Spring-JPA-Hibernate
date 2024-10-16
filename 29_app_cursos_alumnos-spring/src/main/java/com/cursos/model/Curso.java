ef831903eded4915af725fc872a53bc79de5e6a7
Adri <adri86vigo@gmail.com>
2024-10-16 20:39:33 +0200

    PERSISTENCIA - Ejercicio vid 89 (Creación de entidades relacionadas)

---
+++ 29_app_cursos_alumnos-spring/src/main/java/com/cursos/model/Curso.java
@@ -0,0 +1,79 @@
+package com.cursos.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="cursos")
+@NamedQuery(name="Curso.findByDuracion", 
+			query="select c from Curso c where c.duracion<=?1")
+public class Curso {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idCurso;
+	private String denominacion;
+	private int duracion;
+	private double precio;
+	@OneToMany(mappedBy = "curso")
+	private List<Alumno> alumnos;
+	
+	public Curso(int idCurso, String denominacion, int duracion, double precio) {
+		super();
+		this.idCurso = idCurso;
+		this.denominacion = denominacion;
+		this.duracion = duracion;
+		this.precio = precio;
+	}
+
+	public Curso() {
+		super();
+	}
+
+	public int getIdCurso() {
+		return idCurso;
+	}
+
+	public void setIdCurso(int idCurso) {
+		this.idCurso = idCurso;
+	}
+
+	public String getDenominacion() {
+		return denominacion;
+	}
+
+	public void setDenominacion(String denominacion) {
+		this.denominacion = denominacion;
+	}
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public List<Alumno> getAlumnos() {
+		return alumnos;
+	}
+
+	public void setAlumnos(List<Alumno> alumnos) {
+		this.alumnos = alumnos;
+	}
+		
+}
